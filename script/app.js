c0e922047a1c0ed5a05f5b67487455a321dcf7ea
Niels Onderbeke <niels.onderbeke@student.howest.be>
2020-11-16 10:40:37 +0100

    javascript overzichtelijker maken

--- script/app.js
+++ script/app.js
@@ -1,5 +1,21 @@
 "use strict";
 
+//#region *** Global Functions ***
+
+const getCapacityColor = function (availableSpace) {
+  if (availableSpace <= 50) {
+    return "u-space-left--small";
+  } else if (availableSpace <= 150) {
+    return "u-space-left--medium";
+  } else {
+    return "u-space-left--large";
+  }
+};
+
+//#endregion
+
+//#region *** Map Visualisation ***
+
 const provider =
   "https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png";
 const copyright =
@@ -9,15 +25,8 @@ let map, layergroup;
 const maakMarker = function (coords, adres, name, placesLeft, lastUpdate) {
   // console.log(coords)
   const lastUpdateTime = new Date(lastUpdate);
-  lastUpdateTime.setHours((lastUpdateTime.getHours()-2));
-  let colorClass;
-  if (placesLeft <= 50) {
-    colorClass = "u-space-left--small";
-  } else if (placesLeft <= 150) {
-    colorClass = "u-space-left--medium";
-  } else {
-    colorClass = "u-space-left--large";
-  }
+  lastUpdateTime.setHours(lastUpdateTime.getHours() - 2);
+  const colorClass = getCapacityColor(placesLeft);
   const arr_coords = coords;
   layergroup.clearLayers();
   let marker = L.marker(arr_coords).addTo(layergroup);
@@ -45,6 +54,21 @@ const showPointers = function (records) {
   }
 };
 
+const getMap = async function () {
+  let endpoint = `https://data.stad.gent/api/records/1.0/search/?dataset=bezetting-parkeergarages-real-time&q=&rows=100&facet=description`;
+  try {
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    console.log(data);
+    showPointers(data.records);
+  } catch (error) {
+    console.error("An error occured, we handled it.", error);
+  }
+};
+
+//#endregion
+
+//#region *** Table Visualisation ***
 const showTable = function (records) {
   let htmlTable = `<tr class="c-table__table-row c-table_table-row--header">
   <th class="c-table__table-header c-table__col--md">Parking</th>
@@ -55,14 +79,7 @@ const showTable = function (records) {
   </th>
 </tr>`;
   for (const record of records) {
-    let colorClass;
-    if (record.fields.availablecapacity <= 50) {
-      colorClass = "u-space-left--small";
-    } else if (record.fields.availablecapacity <= 150) {
-      colorClass = "u-space-left--medium";
-    } else {
-      colorClass = "u-space-left--large";
-    }
+    const colorClass = getCapacityColor(record.fields.availablecapacity);
     htmlTable += ` <tr class="c-table__table-row">
     <td class="c-table__table-data u-pr__md c-table__col--md" >${record.fields.name}</td>
     <td class="c-table__table-data u-text-align--right c-table__col--sm ${colorClass}">${record.fields.availablecapacity}</td>
@@ -70,6 +87,20 @@ const showTable = function (records) {
   }
   document.querySelector(".c-table").innerHTML = htmlTable;
 };
+
+const getTable = async function () {
+  let endpoint = `https://data.stad.gent/api/records/1.0/search/?dataset=bezetting-parkeergarages-real-time&q=&rows=100&facet=description`;
+  try {
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    console.log(data);
+    showTable(data.records);
+  } catch (error) {
+    console.error("An error occured, we handled it.", error);
+  }
+};
+
+//#endregion
 
 const listenToToggle = function () {
   const mapInput = document.querySelector(".c-toggle-option__input--map"),
@@ -92,30 +123,6 @@ const listenToToggle = function () {
         getTable();
       }
     });
-  }
-};
-
-const getMap = async function () {
-  let endpoint = `https://data.stad.gent/api/records/1.0/search/?dataset=bezetting-parkeergarages-real-time&q=&rows=100&facet=description`;
-  try {
-    const response = await fetch(endpoint);
-    const data = await response.json();
-    console.log(data);
-    showPointers(data.records);
-  } catch (error) {
-    console.error("An error occured, we handled it.", error);
-  }
-};
-
-const getTable = async function () {
-  let endpoint = `https://data.stad.gent/api/records/1.0/search/?dataset=bezetting-parkeergarages-real-time&q=&rows=100&facet=description`;
-  try {
-    const response = await fetch(endpoint);
-    const data = await response.json();
-    console.log(data);
-    showTable(data.records);
-  } catch (error) {
-    console.error("An error occured, we handled it.", error);
   }
 };
 
